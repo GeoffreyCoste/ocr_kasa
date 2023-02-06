fd5d7bcc77533732670a18bb69bf764148bb978a
Geoffrey Coste <geoffreycoste13@gmail.com>
2023-02-06 21:59:18 +0100

    Homepage desktop created

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Logo from '../../assets/logo_white.png';
+import './style.scss';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <img src={Logo} className="logo-white" alt="Kasa logo blanc" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Card from '../../components/Card';
+import accommodations from '../../utils/datas/accomodations.json';
+import './style.scss';
 
 const Home = () => {
-  return <main>Accueil</main>;
+  return (
+    <main>
+      <div className="banner">
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
+      <section id="accommodations">
+        <ul className="accommodations-list">
+          {accommodations.map((a, index) => (
+            <li
+              className="accommodation-item"
+              key={`${a.id}-90984766-ee5c-475f-9718-f3834f0f1e55`}
+            >
+              <Card id={a.id} title={a.title} cover={a.cover} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
