0dab2d392328fb9108a585bcef4ab986a98261cf
Geoffrey Coste <geoffreycoste13@gmail.com>
2023-04-01 15:07:08 +0200

    path typo inside routing rectified

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -8,11 +8,11 @@ import Error from '../Error';
 const Router = () => {
   return (
     <Routes>
-      <Route path="/ocr__kasa/" element={<Home />} />
-      <Route path="/ocr__kasa/about" element={<About />} />
-      <Route path="/ocr__kasa/apartment/:id" element={<Apartment />} />
-      <Route path="/ocr__kasa/apartment/*" element={<Error />} />
-      <Route path="/ocr__kasa/error" element={<Error />} />
+      <Route path="/ocr_kasa/" element={<Home />} />
+      <Route path="/ocr_kasa/about" element={<About />} />
+      <Route path="/ocr_kasa/apartment/:id" element={<Apartment />} />
+      <Route path="/ocr_kasa/apartment/*" element={<Error />} />
+      <Route path="/ocr_kasa/error" element={<Error />} />
       <Route path="*" element={<Error />}></Route>
     </Routes>
   );
